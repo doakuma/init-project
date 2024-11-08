70ba8dee7b071dd5b5cdf00cee6964cc8db7fcdb
doakuma <sg.jeong@insplanet.co.kr>
2024-11-08 10:43:53 +0900

    [dev] component metadata file 분리

---
+++ src/components/metadata/componentList.js
@@ -0,0 +1,127 @@
+export const componentList = [
+  {
+    label: "Basic",
+    lists: [
+      {
+        label: "CustomButton",
+        isDone: true,
+        type: "custombutton",
+      },
+      {
+        label: "CustomIcon",
+        isDone: false,
+        type: "customicon",
+      },
+      {
+        label: "CustomChip",
+        isDone: true,
+        type: "customchip",
+      },
+      {
+        label: "CustomBadge",
+        isDone: false,
+        type: "custombadge",
+      },
+      {
+        label: "CustomModal",
+        isDone: true,
+        type: "custommodal",
+      },
+      {
+        label: "CustomDatagrid",
+        isDone: true,
+        type: "customdatagrid",
+      },
+      {
+        label: "CustomTable",
+        isDone: true,
+        type: "customtable",
+      },
+      {
+        label: "CustomAccordion",
+        isDone: false,
+        type: "customaccordion",
+      },
+      {
+        label: "CustomTab",
+        isDone: true,
+        type: "customtab",
+      },
+      {
+        label: "CustomTreeView",
+        isDone: false,
+        type: "customtreeview",
+      },
+
+      {
+        label: "CustomAvatar",
+        isDone: false,
+        type: "customavatar",
+      },
+    ],
+  },
+  {
+    label: "Forms",
+    lists: [
+      {
+        label: "CustomInput",
+        isDone: true,
+        type: "custominput",
+      },
+      {
+        label: "CustomRadio",
+        isDone: true,
+        type: "customradio",
+      },
+      {
+        label: "CustomCheckbox",
+        isDone: true,
+        type: "customcheckbox",
+      },
+      {
+        label: "CustomSelect",
+        isDone: true,
+        type: "customselect",
+      },
+      {
+        label: "CustomSwitch",
+        isDone: true,
+        type: "customswitch",
+      },
+      {
+        label: "CustomDatepicker",
+        isDone: false,
+        type: "customdatepicker",
+      },
+      {
+        label: "CustomTimepicker",
+        isDone: false,
+        type: "customtimepicker",
+      },
+      {
+        label: "CustomSlider",
+        isDone: false,
+        type: "customslider",
+      },
+      {
+        label: "CustomScheduler",
+        isDone: true,
+        type: "customscheduler",
+      },
+    ],
+  },
+  {
+    label: "Layouts",
+    lists: [
+      {
+        label: "CustomSearch",
+        isDone: true,
+        type: "customsearch",
+      },
+    ],
+  },
+  {
+    label: "Util Function",
+    lists: [],
+  },
+];
